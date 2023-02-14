fdbdf5ddb0cd0d25005c09d10a22ba1ecd887ac1
Garyopen <garyopen1876@gmail.com>
2023-02-14 13:21:12 +0800

    update post_message

--- controller/message.js
+++ controller/message.js
@@ -1,11 +1,18 @@
+const fs = require('fs');
 const data = require("../message.json");
+
+
 
 const get_message = async (_req, res) => {
     return res.status(200).json({ data: data });
 };
 
-const post_message = async (_req, res) => {
-    return res.status(200).json({ message: "User does not exist" });
+const post_message = async (req, res) => {
+    data.push({ "title": req.body.title, "date": req.body.date })
+    fs.writeFile("message.json", JSON.stringify(data), (err) => {
+        if (err) console.log(err);
+    })
+    return res.status(200).json({ message: "OK" });
 };
 
 
