4e3edea6d612cf6e598b1724b6eac43841d35144
Garyopen <garyopen1876@gmail.com>
2023-02-15 08:02:14 +0800

    delete post_message & update message.json

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const message_controller = require("../controller/message.js");
 module.exports = function (router) {
     router.get("/", device_controller.device_check);
     router.get("/message", message_controller.get_message);
-    router.post("/message", message_controller.post_message);
+    // router.post("/message", message_controller.post_message);
 };
